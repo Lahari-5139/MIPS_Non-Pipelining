5dbc5a29c24500e4f0a3fba387025191732f0811
SlanthacK <32712020+SarthakKhoche@users.noreply.github.com>
2018-11-24 10:51:36 +0530

    Update code.c

--- code.c
+++ code.c
@@ -2,24 +2,9 @@
 
 void main()
 {
-  int a[3][3], b[3][3], c[3][3];
-  for(int i=0;i<3;i++)
-  {
-    for(int j=0;j<3;j++)
-    {
-       printf("Enter a[%d][%d] element: ", i, j);
-       scanf("%d", &a[i][j]);
-    }
-  }
-  printf("\n");
-  for(int i=0;i<3;i++)
-  {
-    for(int j=0;j<3;j++)
-    {
-       printf("Enter b[%d][%d] element: ", i, j );
-       scanf("%d", &b[i][j]);
-    }
-  }
+  int a[3][3] = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+  int b[3][3] = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+  int c[3][3];
   for(int i=0;i<3;i++)
   {
     for(int j=0;j<3;j++)
@@ -28,12 +13,5 @@ void main()
       for(int k=0;k<3;k++)
         c[i][j] = c[i][j] + a[i][k] * b[k][j];
     }
-  }
-  printf("\n");
-  for(int i=0;i<3;i++)
-  {
-    for(int j=0;j<3;j++)
-        printf("%d ", c[i][j]);
-    printf("\n");
   }
 }

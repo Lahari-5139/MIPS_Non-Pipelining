2e9cf9f8fbde87f1afa32e7f68a6b7bea268f638
Advait Lonkar <advait.lonkar@iiitb.org>
2018-11-24 15:18:15 +0530

    Debugged

--- jump_control_unit.v
+++ jump_control_unit.v
@@ -7,13 +7,5 @@ input[3:0] func;
 output jumpcontrol;
 
 assign jumpcontrol = ({alu_op,func} == 6'b001000) ? 1'b1 : 1'b0;
-/*if({alu_op,func}==6'b001000)
-begin
-  assign jumpcontrol = 1;
-end
-else
-begin
-  assign jumpcontrol = 0;
-end
-*/
+
 endmodule

5ed0321936bd50989df2dae1fa7015bf235877cc
Advait Lonkar <advait.lonkar@iiitb.org>
2018-11-25 21:43:30 +0530

    Alu added

--- MIPS.v
+++ MIPS.v
@@ -1,4 +1,4 @@
-//Main module
+/*MAIN MODULE*/
 module mips(clk, reset,result);
 
 input clk;
@@ -6,7 +6,7 @@ input reset;
 
 output result;
 
-//Program Counter
+/*PROGRAM COUNTER*/
 reg[15:0] pc;
 wire[15:0] pc_next;
 
@@ -18,13 +18,45 @@ begin
         pc <= pc_next;
 end
 
+/*INSTRUCTION MEMORY*/
 wire[31:0] instruction;
 instruction_memory IM(clk,pc,instruction);
 
-wire  
+
+/*CONTROL UNIT*/
+//Wires required
+wire reg_dst, memto_reg, jump, branch, mem_read, mem_write, alu_src, reg_write, sign_or_zero;
+wire[1:0] alu_op;
+//Module
 control_unit Control(clk,instruction[31:26],reg_dst,memto_reg,alu_op,jump,branch,mem_read,mem_write,alu_src,reg_write,sign_or_zero);  
 
 
+/*REGISTER FILE*/
+//Wires required
+wire[4:0] write_reg;
+wire[15:0] write_data,read_data1,read_data2;
+wire reg_write;
+//Mux for destination register
+assign write_reg = (reg_dst == 1) ? instruction[15:11]:instruction[20:16];  
+//Module 
+register_file Registers(clk,instruction[25:21],instruction[20:16],write_reg,write_data,reg_write,read_data1,read_data2);
+
+
+/*ALU CONTROL*/
+//Wires required
+wire[1:0] alu_op;
+wire[3:0] alu_control;
+//Module
+alu_control_unit ALU_control(clk,instruction[5:0],alu_op,alu_control);
+
+
+/*ALU*/
+//Wires required
+wire [31:0] data_2;
+//Mux for ALU input
+assign data_2 = (alu_src == 1) ?  
+//Module
+alu ALU(clk,read_data1,)
 
 
 
---
+++ alu.v
@@ -0,0 +1,21 @@
+module alu(clk,data_1,data_2,alu_cntrl,alu_out,zero);
+    input[3:0]alu_cntrl;
+    input clk;
+    input[31:0]data_1, data_2;
+    output reg[31:0] alu_out;
+    output reg zero;
+
+    always@(posedge clk)
+    begin
+        zero = 0;
+        case(alu_cntrl)
+            4'b0000: alu_out <= data_1 & data_2;
+            4'b0001: alu_out <= data_1 | data_2;
+            4'b0010: alu_out <= data_1 + data_2;
+            4'b0011: alu_out <= data_1 - data_2;
+            4'b0100: alu_out <= data_1 * data_2;
+        endcase
+        if(alu_out == 0)
+            zero = 1;
+    end
+endmodule

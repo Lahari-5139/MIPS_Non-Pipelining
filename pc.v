c312847edbee448d013e9bff20becd55e0063700
Vishnu Lahari <32712308+Lahari-5139@users.noreply.github.com>
2018-11-26 15:26:30 +0530

    no pc module anymore

--- pc.v
+++ pc.v
@@ -1,32 +1,34 @@
-module pc(clk,reset,extended,branch,zero_flag,pc,pc_nxt);
+// module pc(clk,reset,extended,branch,zero_flag,pc,pc_nxt);
 
-input clk;
-input reset;
-input[31:0]extended;
-input branch,zero_flag;
-input[31:0] pc;
-output[31:0] pc_nxt;
+// input clk;
+// input reset;
+// input[31:0]extended;
+// input branch,zero_flag;
+// input[31:0] pc;
+// output[31:0] pc_nxt;
 
-wire[31:0] adder1;
-wire[31:0] adder2;
-wire[31:0] nxt;
+// wire[31:0] adder1;
+// wire[31:0] adder2;
+// wire[31:0] nxt;
 
 
 
-always @(posedge clk)
-begin
-    if(reset)
-        pc_nxt <= 32'd0;
-    else
-        adder1 = pc+1;
-        extended = extended << 2;
-        adder2 = adder1+extended;
-        wire sel;
-        sel = branch & zero_flag;
-        pc = sel ? adder2 : adder1;
-        pc_nxt <= pc;
+// always @(posedge clk)
+// begin
+//     if(reset)
+//         pc_nxt <= 32'd0;
+//     else
+//         adder1 = pc+1;
+//         extended = extended << 2;
+//         adder2 = adder1+extended;
+//         wire sel;
+//         sel = branch & zero_flag;
+//         pc = sel ? adder2 : adder1;
+//         pc_nxt <= pc;
         
-end
-endmodule
+// end
+// endmodule
 
+
+/*NO PC MODULE. ONLY IN MIPS.v*/
  

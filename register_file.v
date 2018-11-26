47d3b88ba6366b0909a7141f72999f7343ea1f17
Vishnu Lahari <32712308+Lahari-5139@users.noreply.github.com>
2018-11-26 15:27:40 +0530

    updated register file no compiler errors now

--- register_file.v
+++ register_file.v
@@ -4,17 +4,20 @@ input clk;
 input[4:0] read_reg1;
 input[4:0] read_reg2;
 input[4:0] write_reg;
-input[15:0] write_data;
+input[31:0] write_data;
 input reg_write;
-output[15:0] read_data1;
-output[15:0] read_data2;
+output[31:0] read_data1;
+output[31:0] read_data2;
+
+reg[31:0] read_data1;
+reg[31:0] read_data2;
 
 wire[4:0] read_reg1;
 wire[4:0] read_reg2;
 wire[4:0] write_reg;
-reg[15:0] write_data;
-reg[15:0] read_data1;
-reg[15:0] read_data2;
+// reg[15:0] write_data;
+// reg[15:0] read_data1;
+// reg[15:0] read_data2;
 
 // reg[31:0] s0;
 // reg[31:0] s1;
@@ -66,7 +69,7 @@ begin
   end
 end
 
-
+//$display("%d %d",all_registers[0],all_registers[1]);
 
 endmodule
 
@@ -90,3 +93,4 @@ endmodule
 //         endcase
 //     end
 // endmodule
+

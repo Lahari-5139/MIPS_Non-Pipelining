ac7af082a467569cda97df174eb0b29d669ea7b1
Vishnu Lahari <32712308+Lahari-5139@users.noreply.github.com>
2018-11-26 15:25:06 +0530

    updated main_tb no compiler errors now

--- main_tb.v
+++ main_tb.v
@@ -2,11 +2,15 @@ module main_tb;
 
 reg clk;
 reg reset;
+wire[31:0] result;
+wire[31:0] pc_nxt;
 
 mips uut
 (
     .clk(clk),
-    .reset(reset)
+    .reset(reset),
+    .result(result),
+    .pc_nxt(pc_nxt)
 );
 
 initial
@@ -18,12 +22,49 @@ begin
   #5;
   reset = 1;
   clk= 1;
+//   $display("%d",result);
+//   $display("%d",pc_nxt);
   #5;
 
   reset = 0;
   clk = 0;
   #5;
   clk = 1;
+//   $display("%d",result);
+//   $display("%d",pc_nxt);
   #5;
   
+  reset = 0;
+  clk = 0;
+  #5;
+  clk = 1;
+//   $display("%d",result);
+//   $display("%d",pc_nxt);
+  #5;
+
+  reset = 0;
+  clk = 0;
+  #5;
+  clk = 1;
+//   $display("%d",result);
+//   $display("%d",pc_nxt);
+  #5;
+
+  reset = 0;
+  clk = 0;
+  #5;
+  clk = 1;
+//   $display("%d",result);
+//   $display("%d",pc_nxt);
+  #5;
+
+  reset = 0;
+  clk = 0;
+  #5;
+  clk = 1;
+//   $display("%d",result);
+//   $display("%d",pc_nxt);
+  #5;
 end
+
+endmodule
